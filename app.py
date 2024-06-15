d6bec779c134ec7ea2145c6396be60066d8f68d4
Tom Smith <tom.smith@snowflake.com>
2024-06-15 08:46:22 -0600

    Update app.py

--- app.py
+++ app.py
@@ -63,4 +63,4 @@ def prediction():
  
 # Driver code
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000) # IF 5000 IS IN USE, CHOOSE A DIFFERENT ONE. MUST BE UPDATED IN YOUR SERVICE CREATION SPEC
